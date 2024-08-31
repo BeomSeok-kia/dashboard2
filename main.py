b849591d087a2a1ddd536ef087e00519a5a762f9
BeomSeok-kia <kbs02110@naver.com>
2024-08-31 16:45:00 +0900

    주가 차트 추가

--- main.py
+++ main.py
@@ -4,20 +4,21 @@ from data_loader import load_data
 
 def main():
     st.title('대시보드')
-
-df = load_data()
-#	특정날짜범위선택
-st.subheader("Select Date Range")
-df['Date'] =pd.to_datetime(df['Date'])
-with st.expander("범위를선택하세요"):
-    col1, col2=st.columns(2)
-    with col1:
-        start_date=st.date_input("시작일", df['Date'].min())
-    with col2:
-        end_date=st.date_input("종료일", df['Date'].max())
-ranged_df=df[(df['Date'] >=pd.to_datetime(start_date)) &(df['Date'] <=pd.to_datetime(end_date))] 
-ranged_df=ranged_df.reset_index(drop=True)
+    df = load_data()
+ # 특정날짜범위선택
+    st.subheader("범위를선택하세요")
+    df['Date'] = pd.to_datetime(df['Date'])
+    start_date = st.date_input("시작일", 
+df['Date'].min())
+    end_date = st.date_input("종료일", df['Date'].max())
+    ranged_df = df[(df['Date'] >=
+pd.to_datetime(start_date)) & (df['Date'] <=
+pd.to_datetime(end_date))]
+    ranged_df = ranged_df.reset_index(drop=True)
 st.table(ranged_df)
+
+st.subheader("주가변동차트")
+st.line_chart(ranged_df.set_index('Date')['Close'])
 
 if __name__=='__main__':
     main()
