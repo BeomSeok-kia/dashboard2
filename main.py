6dff8c98a0e0f7e229b1c24595b33048b84d9815
BeomSeok-kia <kbs02110@naver.com>
2024-08-31 16:31:41 +0900

    '날짜 범위 선택 expander로 교체'

--- main.py
+++ main.py
@@ -5,15 +5,19 @@ from data_loader import load_data
 def main():
     st.title('대시보드')
 
-df =	load_data()
+df = load_data()
 #	특정날짜범위선택
 st.subheader("Select Date Range")
-df['Date'] = pd.to_datetime(df['Date'])
-start_date = st.date_input("시작일", df['Date'].min())
-end_date = st.date_input("종료일", df['Date'].max())
-
-ranged_df= df[(df['Date'] >= pd.to_datetime(start_date)) & (df['Date'] <= pd.to_datetime(end_date))]
-ranged_df = ranged_df.reset_index(drop=True)
+df['Date'] =pd.to_datetime(df['Date'])
+with st.expander("범위를선택하세요"):
+    col1, col2=st.columns(2)
+    with col1:
+        start_date=st.date_input("시작일", df['Date'].min())
+    with col2:
+        end_date=st.date_input("종료일", df['Date'].max())
+ranged_df=df[(df['Date'] >=pd.to_datetime(start_date)) &(df['Date'] <=pd.to_datetime(end_date))] 
+ranged_df=ranged_df.reset_index(drop=True)
 st.table(ranged_df)
-if __name__==	'__main__':
+
+if __name__=='__main__':
     main()
